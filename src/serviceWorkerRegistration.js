dfea3f2278d84e23fa29b859d63e720cf13e6fee
Jorge Bavaresco <jlbavaresco@gmail.com>
2025-09-08 15:45:53 -0300

    primeira versão

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -71,7 +71,7 @@ function registerValidSW(swUrl, config) {
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.'
               );
-
+              alert('Nova versão disponível! Feche todas as janelas ou abas da aplicação e acesse novamente!')
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
